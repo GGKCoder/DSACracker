ff1ad6288d1f1df038c36acfebbe1888fcd5592c
GGKCoder <coderggk@gmail.com>
2024-06-17 10:32:01 +0530

    adding test file

--- JAVA-DSA/src/Main.java
+++ JAVA-DSA/src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Person person = new Person();
+        person.Name = "Balayya";
+        System.out.println("Hello world "+person.Name);
     }
 }
